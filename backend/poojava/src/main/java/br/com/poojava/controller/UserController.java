64f42b027fdf6ce075a99ba125a59648899ed8b8
Wallace Barros da Cruz <104733992+Wallacebarros@users.noreply.github.com>
2024-04-18 14:58:09 +0000

    implementando o read de um unico usuario atraves do email, e pequenas alteração no delete

--- backend/poojava/src/main/java/br/com/poojava/controller/UserController.java
+++ backend/poojava/src/main/java/br/com/poojava/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.poojava.models.UserModel;
-import br.com.poojava.repository.UserRespository;
 import br.com.poojava.service.UserService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    
-    @Autowired
-    private UserRespository repository;
 
     @Autowired
     private UserService service;
@@ -39,18 +36,29 @@ public class UserController {
         return ResponseEntity.ok().body(users);
     }
 
+    @GetMapping("/{email}")
+    public ResponseEntity<?> readOne(@PathVariable String email){
+        UserModel user = service.getUser(email);
+
+        if (user == null) {
+            return ResponseEntity.badRequest().body("Usuario não encontrado");
+        }
+
+        return ResponseEntity.ok(service.getuser(email));
+    }
+
     @PostMapping
     public ResponseEntity<?> postuser(@RequestBody @Valid UserModel user) {
         return ResponseEntity.ok().body(service.postUser(user));
     }
 
-    @PutMapping
-    public ResponseEntity<?> putUser(@RequestBody @Valid UserModel userBody, @RequestParam(value = "email") String email) {
+    @PutMapping("/{email}")
+    public ResponseEntity<?> putUser(@RequestBody @Valid UserModel userBody, @PathVariable String email) {
         return ResponseEntity.ok().body(service.putUser(userBody, email));
     }
 
     @DeleteMapping
     public ResponseEntity<?> deleteUser(@RequestParam(value = "email") String email) {
-        return ResponseEntity.ok().body(repository.deleteByEmail(email));
+        return ResponseEntity.ok().body(service.deleteUser(email));
     }
 }
